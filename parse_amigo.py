e4bda1ad3024be99c25f91236afee46e42e43c04
Emre Guney <emreguney@gmail.com>
2018-01-19 16:22:45 +0100

    bug fixes on localization parsers

--- parse_amigo.py
+++ parse_amigo.py
@@ -3,32 +3,39 @@ import parse_ncbi, parse_uniprot
 
 def main():
     mapping_file = "/home/emre/data/uniprot/idmapping.tab"
-    uniprot_to_geneid = parse_uniprot.get_uniprot_to_geneid(uniprot_file, uniprot_ids=None)
+    uniprot_to_geneid = parse_uniprot.get_uniprot_to_geneid(mapping_file, uniprot_ids=None)
     file_name = "/home/emre/data/tissue/amigo_extracellular.tsv"
     #file_name = "/home/emre/data/tissue/amigo_membrane.tsv"
     geneid_to_localization = get_geneid_to_localization(file_name, mapping_file)
-    print geneid_to_localization["124912"]
+    print geneid_to_localization["5594"] # MAPK1
     return
 
 
-def get_geneid_to_localization(file_name, file_name_mapping):
-    name_to_geneid = parse_uniprot.get_uniprot_to_geneid(uniprot_file, uniprot_ids=None)
+def get_geneid_to_localization(file_name, mapping_file):
+    name_to_geneid = parse_uniprot.get_uniprot_to_geneid(mapping_file, uniprot_ids=None)
     geneid_to_localization = {}
+    uniprots_unmatched = set()
     for line in open(file_name):
 	words = line.strip("\n").split("\t")
 	uniprot, go, evidence = words[0], words[3], words[7]
-	#if evidence not in ("IDA", etc):
+	idx = uniprot.find(":")
+	uniprot = uniprot[idx+1:]
+	#if evidence not in ("EXP", "IDA", "IPA", "IMP", "IGI", "IEP", "HDA"): 
 	#    continue
 	if uniprot in name_to_geneid:
 	    geneid = name_to_geneid[uniprot]
 	else:
-	    print "Unmatched id", uniprot
-	#! keep evidence type
-	geneid_to_localization.setdefault(geneid, set()).add(go)
+	    #print "Unmatched id", uniprot
+	    if not uniprot.startswith("URS"):
+		uniprots_unmatched.add(uniprot)
+	    continue
+	# Store evidence type as well
+	geneid_to_localization.setdefault(geneid, set()).add((go, evidence))
+    print len(uniprots_unmatched), "Unmatched:", ", ".join(sorted(uniprots_unmatched))
     return geneid_to_localization
 
 
-def get_geneid_to_predicted_localization(file_name_membrane, file_name_secreted, file_name_mapping):
+def get_geneid_to_extracellular_localization(file_name_membrane, file_name_secreted, mapping_file):
     geneid_to_localization = get_geneid_to_localization(file_name_membrane, mapping_file)
     geneid_to_localization2 = get_geneid_to_localization(file_name_extracellular, mapping_file)
     for geneid, localization in geneid_to_localization2.iteritems():
--- parse_hpa.py
+++ parse_hpa.py
@@ -5,45 +5,58 @@ def main():
     mapping_file = "/home/emre/data/ncbi/geneid_to_symbol.txt"
     geneid_to_names, name_to_geneid = parse_ncbi.get_geneid_symbol_mapping(mapping_file)
     file_name = "/home/emre/data/tissue/hpa_subcellular_location.tsv"
-    geneid_to_localization = get_geneid_to_localization(file_name, mapping_file)
-    print geneid_to_localization["124912"]
+    geneid_to_localization = get_geneid_to_localization(file_name, mapping_file) # , only_extracellular=True
+    print geneid_to_localization["8648"] # NCOA1
+    print geneid_to_localization["207"] # AKT1
     file_name_membrane = "/home/emre/data/tissue/hpa_predicted_membrane.tsv"
     file_name_secreted = "/home/emre/data/tissue/hpa_predicted_secreted.tsv"
     geneid_to_localization = get_geneid_to_predicted_localization(file_name_membrane, file_name_secreted, mapping_file)
+    print geneid_to_localization["5925"] # RB1
     return
 
 
-def get_geneid_to_localization(file_name, file_name_mapping):
+def get_geneid_to_localization(file_name, mapping_file, only_extracellular=False):
     """
     Relevant extracellular term: Plasma membrane (GO:0005886)
     Some other terms: Vesicles (GO:0043231);Nuclear bodies (GO:0016604);Nucleoplasm (GO:0005654);Cytosol (GO:0005829);Nucleoli (GO:0005730);Mitochondria (GO:0005739);Cell Junctions (GO:0030054);Endoplasmic reticulum (GO:0005783);Centrosome (GO:0005813);Nucleoli fibrillar center (GO:0001650);Golgi apparatus (GO:0005794);Nuclear speckles (GO:0016607)
+    Reliability types: Uncertain | Approved | Supported | Enhanced
     """
     geneid_to_names, name_to_geneid = parse_ncbi.get_geneid_symbol_mapping(mapping_file)
     geneid_to_localization = {}
+    ids_unmatched = set()
     f = open(file_name)
     f.readline()
     for line in f:
 	words = line.strip("\n").split("\t")
-	gene, reliability, go = words[1], words[2], words[-1]
-	if reliability == "Uncertain":
-	    continue
+	# Evidence and reliability are for experimental characterization
+	gene, reliability, go_terms = words[1], words[2], words[-1]
+	#if reliability == "Uncertain":
+	#    continue
 	if gene in name_to_geneid:
 	    geneid = name_to_geneid[gene]
 	else:
-	    print "Unmatched id", gene
-	idx = go.find("(")
-	go = go[idx+1:-1]
-	geneid_to_localization.setdefault(geneid, set()).add(go)
+	    #print "Unmatched id", gene
+	    ids_unmatched.add(gene)
+	    continue
+	for go in go_terms.split(";"):
+	    idx = go.find("(")
+	    go = go[idx+1:-1]
+	    if only_extracellular and go != "GO:0005886":
+		continue
+	    geneid_to_localization.setdefault(geneid, set()).add((go, reliability))
     f.close()
+    print len(ids_unmatched), "Unmatched:", ", ".join(sorted(ids_unmatched))
     return geneid_to_localization
 
 
-def get_geneid_to_predicted_localization(file_name_membrane, file_name_secreted, file_name_mapping):
+def get_geneid_to_predicted_localization(file_name_membrane, file_name_secreted, mapping_file):
     """
     Relevant protein classes: Predicted secreted proteins | Predicted membrane proteins
+    Note that evidence and reliability are for experimental characterization
     """
     geneid_to_names, name_to_geneid = parse_ncbi.get_geneid_symbol_mapping(mapping_file)
     geneid_to_localization = {}
+    ids_unmatched = set()
     f = open(file_name_membrane)
     f.readline()
     for line in f:
@@ -54,22 +67,32 @@ def get_geneid_to_predicted_localization(file_name_membrane, file_name_secreted,
 	if gene in name_to_geneid:
 	    geneid = name_to_geneid[gene]
 	else:
-	    print "Unmatched id", gene
+	    #print "Unmatched id", gene
+	    ids_unmatched.add(gene)
+	    continue
 	for localization in protein_class.split(", "):
-	    geneid_to_localization.setdefault(geneid, set()).add(localization)
+	    localization = localization.replace("Predicted ", "").replace(" proteins", "").replace(" genes", "")
+	    if localization == "membrane":
+		geneid_to_localization.setdefault(geneid, set()).add((localization, reliability))
     f.close()
     f = open(file_name_secreted)
     f.readline()
     for line in f:
 	words = line.strip("\n").split("\t")
+	# Evidence and reliability are for experimental characterization
 	gene, protein_class, evidence, reliability = words[0], words[6], words[7], words[11]
 	if gene in name_to_geneid:
 	    geneid = name_to_geneid[gene]
 	else:
-	    print "Unmatched id", gene
+	    #print "Unmatched id", gene
+	    ids_unmatched.add(gene)
+	    continue
 	for localization in protein_class.split(", "):
-	    geneid_to_localization.setdefault(geneid, set()).add(localization)
+	    localization = localization.replace("Predicted ", "").replace(" proteins", "").replace(" genes", "")
+	    if localization == "secreted":
+		geneid_to_localization.setdefault(geneid, set()).add((localization, reliability))
     f.close()
+    print len(ids_unmatched), "Unmatched:", ", ".join(sorted(ids_unmatched))
     return geneid_to_localization
 
 
